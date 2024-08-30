dda0ffc80fb4e660a0f7c7f486444a089dbcc658
Zekito <duarte_ezequieldd@hotmail.com>
2024-08-29 23:42:53 -0300

    What I do: + Finish the UI structure of my portfolio + New feature: Dark/Light Mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				emerald: {
+					600: '#059669',
+				},
+				cerise: {
+					100: '#fbefea',
+					500: '#d6525f',
+					600: '#be3843',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
